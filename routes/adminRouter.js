4f617481add831882954cdbf5cdcdb4886e44d64
ignacioirigoitia <ignacio.irigoitia@gmail.com>
2021-01-28 17:13:35 -0300

    agregue archivos

---
+++ routes/adminRouter.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+
+const {index, carsList, carsCreate, carsEdit, carsStore, carsDelete, carsUpdate, register, processRegister, login, processLogin} = require('../controllers/adminController');
+
+router.get('/',index);
+
+//entidad administradores
+
+router.get("/register", register)
+router.post("/register", processRegister)
+
+router.get("/login", login)
+router.post("/login", processLogin)
+
+//entidad autos
+
+router.get('/autos/list',carsList);
+
+router.get('/autos/create',carsCreate);
+router.post('/autos/store',carsStore);
+
+router.get('/autos/edit/:id',carsEdit);
+router.put('/autos/update/:id',carsUpdate);
+
+router.delete('/autos/delete/:id',carsDelete);
+
+
+module.exports = router;
